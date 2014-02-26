5c18b3b76eb4297fbeab891fe236f83544bbf31a
markshao <37647536@qq.com>
2014-02-26 16:58:50 +0800

    add the description for the labmanager module

--- setup.py
+++ setup.py
@@ -2,12 +2,18 @@ __author__ = 'root'
 
 from setuptools import setup, find_packages
 
-__version__ = "1.0"
+__version__ = "1.0.1"
 
 setup(
     name="labmanager",
     version=__version__,
     packages=find_packages(),
+    description='the labmanager cloud plugin for pagrant',
+    classifiers=[
+        "Programming Language :: Python",
+    ],
+    platforms='any',
+    keywords='framework testing',
     entry_points={
         'PAGRANT': [
             "VMPROVIDER=labmanager.actions:LabManagerVmProvider",
